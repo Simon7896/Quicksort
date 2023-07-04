6bad038cf1fd5299fb6b5bc10490b50323452d77
simonyu <simonyu180@gmail.com>
2023-07-04 15:06:40 -0700

    Added list to sort

--- quicksort.js
+++ quicksort.js
@@ -1,7 +1,15 @@
 function quicksort(list) {
-
+    
 }
 
-list = []
+list = [
+    "Sort a List", 
+    "Social Media API", 
+    "Try using Bootstrap", 
+    "Create a Browser Extension", 
+    "Redesign Your Favorite Website", 
+    "Build a Data Visualization", 
+    "Explore Hardware(Emulators Welcome!"
+]
 
 console.log(quicksort(list))

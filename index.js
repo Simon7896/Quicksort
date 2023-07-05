2f6fdad40231d40306fbe330afc3479d72ed8741
simonyu <simonyu180@gmail.com>
2023-07-04 17:14:34 -0700

    Fixed bug which added extra list item to output

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function showList(event) {
     quicksort(list, 0, list.length-1)
     console.log(list)
 
+    list.splice(0,1)
     const outputList = document.getElementById('outputList')
     for (i=0; i<list.length; i++) {
         var listItem = document.createElement('li')
